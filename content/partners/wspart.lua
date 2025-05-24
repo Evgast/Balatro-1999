4bb4eda8c0f769f499c9ed60d760b0f9fb1f747a
Evgast <136436468+Evgast@users.noreply.github.com>
2025-05-24 23:22:52 +1100

    Regoofed

--- content/partners/wspart.lua
+++ content/partners/wspart.lua
@@ -7,15 +7,27 @@ Partner_API.Partner{
     pos = {x = 2, y = 0},
     loc_txt = {},
     atlas = "part",
-    config = {extra = { related_card = "j_b1999_ws", mult = 2, mult_bef = 3 }},
+    config = {extra = { related_card = "j_b1999_ws", mult = 2, held = "held in hand" }},
     loc_vars = function(self, info_queue, card)
-        local benefits = 1
-        if next(SMODS.find_card("j_b1999_ws")) then benefits = true end
-        return { vars = { card.ability.extra.mult, card.ability.extra.mult_bef } }
+        local benefits = false
+        card.ability.extra.held = "held in hand"
+        card.ability.extra.mult = 2
+        if next(SMODS.find_card("j_b1999_ws")) then
+            benefits = true
+            card.ability.extra.held = "scored"
+            card.ability.extra.mult = 3
+        end
+        return { vars = { card.ability.extra.mult, card.ability.extra.held } }
     end,
     calculate = function(self, card, context)
         local benefits = false
-        if next(SMODS.find_card("j_b1999_ws")) then benefits = true end
+        card.ability.extra.mult = 2
+        card.ability.extra.held = "held in hand"
+        if next(SMODS.find_card("j_b1999_ws")) then
+            benefits = true
+            card.ability.extra.held = "scored"
+            card.ability.extra.mult = 3
+        end
 		if not context.end_of_round and context.individual and context.cardarea == G.hand and context.other_card:is_suit("Spades") and benefits == false then
             return {
 				card = card,
@@ -25,7 +37,7 @@ Partner_API.Partner{
         if not context.end_of_round and context.individual and context.cardarea == G.play and context.other_card:is_suit("Spades") and benefits == true then
             return {
 				card = card,
-                mult = card.ability.extra.mult_bef
+                mult = card.ability.extra.mult
             }
         end
     end
