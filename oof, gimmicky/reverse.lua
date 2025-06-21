93c0398b2a917b25fb999231da2234946bf0bb1d
Evgast <136436468+Evgast@users.noreply.github.com>
2025-06-21 13:19:13 +1100

    Add files via upload

---
+++ oof, gimmicky/reverse.lua
@@ -0,0 +1,9 @@
+B1999.SPECIL = {}
+
+function SpecilReverse(base, reversed)
+    local specil = {
+        base = base,
+        reversed = reversed
+    }
+    table.insert(B1999.SPECIL, specil)
+end
